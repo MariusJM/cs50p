4b48ccdbc35ed5785a04f954de63aecf852bb402
MariusJM <mariusjm.code@gmail.com>
2023-12-29 12:25:17 +0200

    add signup logic

--- FinalProject/project.py
+++ FinalProject/project.py
@@ -11,9 +11,19 @@ user_password = None
 remember_me = None
 w_login = None
 def main():
-    login_window()
-    mainApp()
-        
+    check_saved_user()
+
+
+def check_saved_user():
+    rememberMe = open(os.path.join(os.getcwd(),r"FinalProject/rememberMe.txt"))
+    # print(rememberMe.read())
+    if rememberMe.readline() != "":
+        rememberMe.close()
+        mainApp()
+    else:
+        rememberMe.close()
+        login_window()
+
 
 def check_credentials(user, password):
     global w_login
@@ -23,23 +33,43 @@ def check_credentials(user, password):
     sheet = client.open('math_users').sheet1
     users = sheet.col_values(1)[1:]
     if user in users:
-        print("user found")
         fuser = sheet.find(user)
-        print("User is in the database")
-
         if sheet.cell(fuser.row, 2).value == password:
-            w_login.destroy()
-            mainApp()
-            return True
+            if remember_me.get() == True:
+                with open(os.path.join(os.getcwd(),r"FinalProject/rememberMe.txt"), "w") as rememberMe_file:
+                    rememberMe_file.write(user)
+                    rememberMe_file.close()
+                    w_login.destroy()
+                    mainApp()
+                    return True
+            else:
+                w_login.destroy()
+                mainApp()
+                return True
         else:
             messagebox.showerror("Error", "Incorrect password")
     else:
         messagebox.showerror("Error", f"User {user} does not exist")
 
+def add_new_user(user, password, password_repeat, signupWindow):
+    if password == password_repeat and password != "":
+        scope =  ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+        creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(os.getcwd(),r"FinalProject/mathapp_client_secret.json"), scope)
+        client = gspread.authorize(creds)
+        sheet = client.open('math_users').sheet1
+        users = sheet.col_values(1)[1:]
+        if user in users:
+            messagebox.showerror("Error", "User allready exists")
+            
+        else:
+            sheet.insert_row([user, password], 2)
+            signupWindow.destroy()
+            login_window()
+    else:
+        messagebox.showerror("Error", "Password does not match")
 
 def login_window():
     global user_name, user_password, remember_me, w_login
-
 
     #login window    
     w_login = ttk.Window(themename="flatly")
@@ -79,16 +109,16 @@ def signup_window():
     ttk.Label(w_signup, text="Signup", font="Calibri 24 bold").pack(pady=20)
     f_signup = ttk.Frame(w_signup, width = 250, height = 400, borderwidth= 1, relief="solid")
     f_signup.pack_propagate(False)
-    user_name = tk.StringVar()
-    user_password = tk.StringVar()
+    user_name_su = tk.StringVar()
+    user_password_su = tk.StringVar()
     user_password_reentry = tk.StringVar()
     l_username = ttk.Label(master=f_signup,text="Email address", anchor="w", justify="left",width=22).pack()
-    e_user_name = ttk.Entry(master=f_signup,justify="left",width=20,textvariable=user_name).pack(padx=10,pady=(2, 10))
+    e_user_name = ttk.Entry(master=f_signup,justify="left",width=20,textvariable=user_name_su).pack(padx=10,pady=(2, 10))
     l_password = ttk.Label(master=f_signup,text="Password", anchor="w", justify="left",width=22).pack()
-    e_password = ttk.Entry(master=f_signup,justify="left",width=20,textvariable=user_password).pack(padx=10,pady=(2, 10))
+    e_password = ttk.Entry(master=f_signup,justify="left",width=20,textvariable=user_password_su).pack(padx=10,pady=(2, 10))
     l_password_reentry = ttk.Label(master=f_signup,text="Repeat Password", anchor="w", justify="left",width=22).pack()
     e_password_reentry = ttk.Entry(master=f_signup,justify="left",width=20,textvariable=user_password_reentry).pack(padx=10,pady=(2, 10))
-    b_sign_up = ttk.Button(master=f_signup, text="Sign up", command= lambda: check_credentials(user_name.get(), user_password.get())).pack(padx=5, pady=5)
+    b_sign_up = ttk.Button(master=f_signup, text="Sign up", command= lambda: add_new_user(user_name_su.get(), user_password_su.get(), user_password_reentry.get(), w_signup)).pack(padx=5, pady=5)
     b_back = ttk.Button(master=f_signup, text="Back", command= lambda:[w_signup.destroy(), login_window()]).pack(padx=5, pady=5)
     f_signup.pack()
     w_signup.mainloop()
